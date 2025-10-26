b47d70c86cc4de2eabb7e71242d085384446f5be
mahendren84 <mahendren84@gmail.com>
2025-10-27 02:10:25 +0900

    Added run_bestmodel.sh to extract best model selection

---
+++ scripts/run_bestmodel.sh
@@ -0,0 +1,18 @@
+python Train.py \
+  --project cs6886-vgg6-cifar10 \
+  --activation gelu \
+  --optimizer sgd_nesterov \
+  --lr 0.07344012714787108 \
+  --momentum 0.9 \
+  --weight_decay 0.00028487295398105526 \
+  --batch_size 128 \
+  --epochs 100 \
+  --scheduler cosine \
+  --auto_augment True \
+  --entity null \
+  --gamma 0.1 \
+  --label_smoothin 0 \
+  --num_workers 2 \
+  --step_size 30 \
+  --seed 42
+  
